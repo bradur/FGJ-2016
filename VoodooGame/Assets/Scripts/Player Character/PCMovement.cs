a8fc9566bc5fd0beeed796514957dd1983a54096
Juho Saarelainen <bradurdung@gmail.com>
2016-01-29 22:07:02 +0200

    :runner: Basic movement with placeholder assets.

---
+++ VoodooGame/Assets/Scripts/Player Character/PCMovement.cs
@@ -0,0 +1,37 @@
+// Date   : 29.01.2016 21:23
+// Project: VoodooGame
+
+using UnityEngine;
+using System.Collections;
+
+public class PCMovement : MonoBehaviour {
+
+    [SerializeField]
+    [Range(0.5f, 5f)]
+    private float speed = 1f;
+
+    [SerializeField]
+    [Range(0.2f, 2f)]
+    private float speedFactor = 0.5f;
+
+    private Vector2 targetSpeed;
+    private Rigidbody2D rigidBody2D;
+
+    void Start () {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float move_h = Input.GetAxis("Horizontal");
+        float move_v = Input.GetAxis("Vertical");
+
+        targetSpeed = new Vector2(speed * move_h, speed * move_v);
+
+        rigidBody2D.AddForce(speedFactor* targetSpeed - rigidBody2D.velocity, ForceMode2D.Impulse);
+    }
+
+    void Update () {
+    
+    }
+}
